b7886c942753a50040bc27b3fc2a1b3cffa009c3
soomin-jeong <soominjeongkr@gmail.com>
2022-11-07 11:38:37 +0900

    added comments on the merge_and_transpose_ratings

--- survey/backend/src/strategies/preprocessing/matrix_builder.py
+++ survey/backend/src/strategies/preprocessing/matrix_builder.py
@@ -7,6 +7,13 @@ class MatrixBuilder:
         self.rating_df = rating_df
         self.rating_matrix = self.merge_and_transpose_ratings(self.rating_df)
 
+    '''
+    This func reformats the ratings dataframe. 
+    Input: a pd.DataFrame whose columns are [userId, movieId, rating, timestamp]
+    Output: a pd.DataFrame whose columns are [userId, (all the movie Ids as columns)... rating]
+
+    This step is necessary for the k-means clustering as it requires all the ratings of one user in one row.    
+    '''
     def merge_and_transpose_ratings(self, rating_df: pd.DataFrame) -> pd.DataFrame:
         unique_users = rating_df['userId'].unique()
         unique_users.sort()
