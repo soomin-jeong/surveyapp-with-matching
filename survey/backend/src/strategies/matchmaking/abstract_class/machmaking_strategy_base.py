cefb1495ba41ddf3580f15d01585ed654cbf8f80
soomin-jeong <soominjeongkr@gmail.com>
2022-12-04 17:10:26 +0000

    added a test for least diff matchmaking strategy

--- survey/backend/src/strategies/matchmaking/abstract_class/machmaking_strategy_base.py
+++ survey/backend/src/strategies/matchmaking/abstract_class/machmaking_strategy_base.py
@@ -1,24 +1,24 @@
-from ...preprocessing.hierarchical_clustering import UserCluster
-from ....app import db
-
 import pandas as pd
+
+from ...preprocessing.hierarchical_clustering import UserCluster
 from abc import abstractmethod, ABCMeta
 
 
 class MatchmakingBase(metaclass=ABCMeta):
 
-    def __init__(self, matched_cluster: UserCluster):
+    def __init__(self, matched_cluster: UserCluster, online_user_rating: dict):
         self.matched_cluster = matched_cluster
+        self.online_user_id = online_user_rating
 
-    def get_matched_offline_user(self) -> int:
+    def get_matched_offline_user_id(self) -> int:
         # when there is only one user in the cluster, match with the user right away
         if self.matched_cluster.user_cnt == 1:
             return self.matched_cluster.user_ids[0]
 
         else:
-            return self.match_one_among_users_in_cluster()
+            return self.get_matched_user_id_among_multiple_in_cluster()
 
     @abstractmethod
-    def match_one_among_users_in_cluster(self) -> int:
+    def get_matched_user_id_among_multiple_in_cluster(self) -> int:
         raise NotImplementedError("{}.{} should be implemented".format(self.__class__.__name__, self.__module__.__name__))
 
--- survey/backend/src/strategies/matchmaking/implemented_strategies/random_matchmaking_strategy.py
+++ survey/backend/src/strategies/matchmaking/implemented_strategies/random_matchmaking_strategy.py
@@ -5,6 +5,6 @@ from backend.src.strategies.matchmaking.abstract_class.machmaking_strategy_base 
 
 class Strategy(MatchmakingBase):
 
-    def match_one_among_users_in_cluster(self) -> int:
+    def get_matched_user_id_among_multiple_in_cluster(self) -> int:
         reproducible_random_state = random.RandomState(97)
         return int(reproducible_random_state.choice(self.matched_cluster.user_ids))
