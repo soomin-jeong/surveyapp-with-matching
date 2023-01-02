4ff39e612434d6e0831f8dc5d545c0b355c4bfe0
SooMin Jeong <soomin.jeong@skyscanner.net>
2023-01-02 19:43:37 +0000

    moved abstract function to parent class

--- survey/backend/src/strategies/next_question_selection/abstract_class/item_selection_base.py
+++ survey/backend/src/strategies/next_question_selection/abstract_class/item_selection_base.py
@@ -59,3 +59,21 @@ class BaseStrategy(metaclass=abc.ABCMeta):
         In this case, regardless of the matching strategy, the offline user should be matched with the online user
         """
         raise NotImplementedError("{}.{} should be implemented".format(self.__class__.__name__, self.__module__.__name__))
+
+    @abc.abstractmethod
+    def simulate_online_user_response(self, online_user_id: int, candidate_item_ids: [int]) -> int:
+        """
+        survey/backend/src/strategies/evaluation.py evaluates the performance of questioning strategies by simulating a user's response
+        as if we did not know this user's ratings, like limiting training data to test it later in Machine Learning.
+        This function simulates an online user's response during evaluation
+        given that the online user's rating is already in the ratings data.
+
+        Args:
+            online_user_id: id of the online user who will be making the decision when given a question
+            candidate_item_ids: ids of items given as options to choose from
+
+        i.e. when the survey application shows 5 items (id = 11, 12, 13, 14, 15),
+        this function will return the id of the item which online user will choose from these items
+        """
+        # returns one movie_id in string
+        raise NotImplementedError("{}.{} should be implemented".format(self.__class__.__name__, self.__module__.__name__))
