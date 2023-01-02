e1682fc6b20786cb2d162ae466cc880b1845ffff
SooMin Jeong <soomin.jeong@skyscanner.net>
2023-01-02 08:53:43 +0000

    added new evaluation results from Wilcoxon test

--- survey/backend/src/strategies/evaluation/eval_runner.py
+++ survey/backend/src/strategies/evaluation/eval_runner.py
@@ -1,8 +1,10 @@
 import time
 
 import numpy as np
-import scipy.stats as stats
+import statistics
 
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 from backend.src.strategies.evaluation.evaluation import EvaluationOfStrategies
 from backend.src.strategies.next_question_selection.implemented_strategies.favorite_item_strategy import \
@@ -30,11 +32,11 @@ def print_eval_result(dataset_name, repeating_times, questioning_strategies, mat
             hit_ratio = ev.get_hit_ratio_of_combination_of_questioning_and_matchmaking_strategy(
                 repeating_times=repeating_times)
             end = time.time()
-            print("Hit Rate (questioning={}, matching={}): \t{} (in {}s)".format(each_questioning_strategy.strategy_name,
+            print("Hit Rate (questioning={}, matching={}): \t{} (in {}s)\n".format(each_questioning_strategy.strategy_name,
                                                                                  each_matching_strategy.strategy_name,
                                                                                  hit_ratio,
                                                                                  round(end-start, 5)))
-    print("")
+
 
 
 def print_t_test_result(dataset_name, matching_strategies, questioning_strategies, sample_size, n_samples):
@@ -63,28 +65,88 @@ def print_t_test_result(dataset_name, matching_strategies, questioning_strategie
     control_hit_ratios = collect_hit_ratio(dataset_name, CONTROL_QUESTIONING_STRATEGY,
                                            CONTROL_MATCHING_STRATEGY, sample_size, n_samples)
 
+    print("1: NORMALITY")
+    control_normality = stats.shapiro(control_hit_ratios)
+    print(f'[{CONTROL_MATCHING_STRATEGY.strategy_name}] normal dist p-val: {control_normality.pvalue}')
+
+    plt.hist(control_hit_ratios, 10)
+    plt.title(
+        f'[q={CONTROL_QUESTIONING_STRATEGY.strategy_name}, m={CONTROL_MATCHING_STRATEGY.strategy_name}]\n (sample_size={sample_size}, n_samples={n_samples})')
+    plt.show()
+
     for each_m_strategy in matching_strategies:
         for each_q_strategy in questioning_strategies:
             test_hit_ratios = collect_hit_ratio(dataset_name, each_q_strategy, each_m_strategy, sample_size, n_samples)
 
-            t_test_result = stats.ttest_ind(a=control_hit_ratios, b=test_hit_ratios, equal_var=True)
+            # Testing Normality
+            test_normality = stats.shapiro(test_hit_ratios)
+            print(f'[{each_q_strategy.strategy_name}] normal dist p-val: {test_normality.pvalue}')
+
+            if control_normality.pvalue >= 0.05:
+                print(f'✅ [{CONTROL_MATCHING_STRATEGY.strategy_name}] is normally distributed')
+            else:
+                print(f'❌ [{CONTROL_MATCHING_STRATEGY.strategy_name}] is NOT normally distributed')
+
+            if test_normality.pvalue >= 0.05:
+                print(f'✅ [{each_q_strategy.strategy_name}] is normally distributed')
+            else:
+                print(f'❌ [{each_q_strategy.strategy_name}] is NOT normally distributed')
+            print('\n')
+
+            plt.hist(test_hit_ratios, 10)
+            plt.title(f'[q={each_q_strategy.strategy_name}, m={each_m_strategy.strategy_name}] (sample_size={sample_size}, n_samples={n_samples})')
+            plt.show()
+
+            # Testing Variance
+            print("2: VARIANCE")
+
+            print(f'[{CONTROL_MATCHING_STRATEGY.strategy_name}] variance: {statistics.variance(control_hit_ratios)}')
+            print(f'[{each_q_strategy.strategy_name}] variance: {statistics.variance(test_hit_ratios)}')
+
+            var_comparison = stats.levene(control_hit_ratios, test_hit_ratios, center='mean').pvalue
+            equal_var = (var_comparison >= 0.05)
+            if equal_var:
+                print(f'✅ Variances are similar (pval < 0.05)')
+            else:
+                print(f'❌ Variances are NOT similar (pval >= 0.05)')
+            print('\n')
+
+            # Hypothesis Test 1
+            ranksum_test = stats.ranksums(control_hit_ratios, test_hit_ratios)
+
             print(
-                f'## T-test result of [q={each_q_strategy.strategy_name}, m={each_m_strategy.strategy_name}] (sample_size={sample_size}, n_samples={n_samples})')
+                f'3. RankSum result of [q={each_q_strategy.strategy_name}, m={each_m_strategy.strategy_name}] (sample_size={sample_size}, n_samples={n_samples})')
+
+            print(f'- Avg hit ratio of ctrl strategy: {round(sum(control_hit_ratios) / len(control_hit_ratios), 3)}')
+            print(f'- Avg hit ratio of test strategy: {round(sum(test_hit_ratios) / len(test_hit_ratios), 3)}')
+
+            if ranksum_test.pvalue < 0.05:
+                print(f'- ✅ Result: REJECT null hypothesis (p-value: {ranksum_test.pvalue})')
+            else:
+                print(f'- ❌ Result: ACCEPT null hypothesis (p-value: {ranksum_test.pvalue})')
+            print('\n')
+
+
+            # Hypothesis Test 2
+            t_test_result = stats.ttest_ind(a=control_hit_ratios, b=test_hit_ratios, equal_var=equal_var)
+            print(
+                f'4. T-test result of [q={each_q_strategy.strategy_name}, m={each_m_strategy.strategy_name}] (sample_size={sample_size}, n_samples={n_samples})')
 
             print(f'- Avg hit ratio of ctrl strategy: {round(sum(control_hit_ratios) / len(control_hit_ratios), 3)}')
             print(f'- Avg hit ratio of test strategy: {round(sum(test_hit_ratios) / len(test_hit_ratios), 3)}')
 
             if t_test_result.pvalue < 0.05:
-                print(f'- Result: REJECT null hypothesis (p-value: {t_test_result.pvalue})\n')
+                print(f'- ✅ Result: REJECT null hypothesis (p-value: {t_test_result.pvalue})')
             else:
-                print(f'- Result: ACCEPT null hypothesis (p-value: {t_test_result.pvalue})\n')
+                print(f'- ❌ Result: ACCEPT null hypothesis (p-value: {t_test_result.pvalue})')
+            print('\n')
 
 
-test_dataset_name = 'ml-1m'
+test_dataset_name = 'movielens_small'
 test_repeating_times = 100
 
 # USER GUIDE: add the  questioning strategies to evaluate here
-questioning_strategies = [questioning_random, questioning_favorite, questioning_rated_most, questioning_maniac]
+questioning_strategies = [questioning_favorite, questioning_rated_most, questioning_maniac]
 # USER GUIDE: add the  matching strategies to evaluate here
 matching_strategies = [matching_random, matching_least_diff]
 
@@ -92,11 +154,14 @@ matching_strategies = [matching_random, matching_least_diff]
 
 
 ## T-test
-SAMPLE_SIZE = 100
-N_SAMPLES = 100
+# SAMPLE_SIZE = 5500
+# N_SAMPLES = 100
+# test_dataset_names = ['movielens_small', 'ml-100k', 'ml-1m']
 
-print_t_test_result(test_dataset_name, matching_strategies, questioning_strategies, SAMPLE_SIZE, N_SAMPLES)
-# print_t_test_result(test_dataset_name, matching_strategies, [questioning_rated_most], SAMPLE_SIZE, N_SAMPLES)
+
+# print_t_test_result('movielens_small', [matching_random], questioning_strategies, 5500, 100)
+# print_t_test_result('ml-100k', [matching_random], questioning_strategies, 7500, 100)
+print_t_test_result('ml-1m', [matching_random], questioning_strategies, 80000, 100)
 
 
 
